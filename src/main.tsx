0617524aca4fd5f2fa1c7e344f7a08e051797a7b
huseynmv <huseynmv@yahoo.com>
2025-05-17 11:21:36 +0400

    structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '../src/styles/style.css'
+import '../src/styles/reset.css'
 import './index.css'
 import App from './App.tsx'
 
